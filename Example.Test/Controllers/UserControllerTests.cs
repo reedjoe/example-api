df7bdeb47133ab507e3b9f9010089487024f3332
Joe Reed <reedjoe36@gmail.com>
2021-03-20 17:22:24 +0000

    Add unit tet project and user controller tests

---
+++ Example.Test/Controllers/UserControllerTests.cs
@@ -0,0 +1,84 @@
+﻿using Example.API.Controllers;
+using Example.Core.Dto;
+using Example.Service.Services;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Microsoft.AspNetCore.Mvc;
+using Moq;
+using System;
+using System.Threading.Tasks;
+
+namespace Example.Test.Controllers
+{
+    [TestClass]
+    public class UserControllerTests
+    {
+        Mock<IUserService> mockUserService;
+
+        [TestInitialize]
+        public void Setup()
+        {
+            this.mockUserService = new Mock<IUserService>();
+        }
+
+        [TestMethod]
+        public async Task UserController_GetUser_ReturnsSuccess()
+        {
+            // Arrange
+            Guid userId = Guid.NewGuid();
+            UserDto user = new UserDto();
+            this.mockUserService
+                .Setup(service => service.GetUser(userId))
+                .ReturnsAsync(user);
+            var sut = this.CreateSut();
+
+            // Act
+            var res = await sut.GetUser(userId);
+
+            //Assert
+            var result = res.Result as OkObjectResult;
+            Assert.AreEqual(result.Value, user);
+        }
+
+        [TestMethod]
+        public async Task UserController_ListUsers_ReturnsSuccess()
+        {
+            // Arrange
+            ListUserDto userList = new ListUserDto();
+            this.mockUserService
+                .Setup(service => service.ListUsers())
+                .ReturnsAsync(userList);
+            var sut = this.CreateSut();
+
+            // Act
+            var res = await sut.ListUsers();
+
+            //Assert
+            var result = res.Result as OkObjectResult;
+            Assert.AreEqual(result.Value, userList);
+        }
+
+        [TestMethod]
+        public async Task UserController_CreateUser_ReturnsSuccess()
+        {
+            // Arrange
+            Guid userId = Guid.NewGuid();
+            CreateUserDto user = new CreateUserDto();
+            this.mockUserService
+                .Setup(service => service.CreateUser(user))
+                .ReturnsAsync(userId);
+            var sut = this.CreateSut();
+
+            // Act
+            var res = await sut.CreateUser(user);
+
+            //Assert
+            var result = res.Result as OkObjectResult;
+            Assert.AreEqual(result.Value, userId);
+        }
+
+        private UserController CreateSut()
+        {
+            return new UserController(this.mockUserService.Object);
+        }
+    }
+}
